1908df7dec28a14447855a2afd66d5d34282560e
ADMIN <ADMIN@DESKTOP-IPDB6V8>
2025-03-12 10:53:56 +0700

    classstudenr

---
+++ THuchanh2/src/main/java/com/mycompany/thuchanh2/THuchanh2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thuchanh2;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class THuchanh2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
